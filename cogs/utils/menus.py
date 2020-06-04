c9347f2b72e353c910259bfdf9d2ed836f5d0055
RMGRich <rmgrichdeveloper@gmail.com>
2020-06-03 23:15:58 -0500

    IM THE BEST

--- cogs/utils/menus.py
+++ cogs/utils/menus.py
@@ -45,31 +45,39 @@ class Paginator(menus.Menu):
         await self.message.edit(embed=self.embeds[self.page])
 
     @menus.button('\u23ea', skip_if=isloop)
-    async def tofirst(self, _unusedpayload):
+    async def tofirst(self, payload):
+        if not payload.member: return
         self.page = 0
         await self.edit()
+        await self.message.remove_reaction("\u23ea", payload.member)
     
     @menus.button('\u25C0')
-    async def left(self, _unusedpayload):
+    async def left(self, payload):
+        if not payload.member: return
         self.page -= 1
         if self.page < 0:
             if self.loop: self.page = self.max
             else: self.page = 0
         await self.edit()
+        await self.message.remove_reaction("\u25C0", payload.member)
 
     @menus.button('\u23F9')
     async def stopbutton(self, _unusedpayload):
         self.stop()
 
     @menus.button('\u25B6')
-    async def right(self, _unusedpayload):
+    async def right(self, payload):
+        if not payload.member: return
         self.page += 1
         if self.page > self.max:
             if self.loop: self.page = 0
             else: self.page = self.max
         await self.edit()
+        await self.message.remove_reaction("\u25B6", payload.member)
     
     @menus.button('\u23e9', skip_if=isloop)
-    async def tolast(self, _unusedpayload):
+    async def tolast(self, payload):
+        if not payload.member: return
         self.page = self.max
         await self.edit()
+        await self.message.remove_reaction("\u25B6", payload.member)
