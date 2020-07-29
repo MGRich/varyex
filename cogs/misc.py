e21627c703576e308040fb15b7f6c0cc80640d77
RMGRich <rmgrichdeveloper@gmail.com>
2020-07-29 17:37:38 -0500

    gggGGGaIRRFLEe,.d.d..d.......

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,14 +1,31 @@
-import discord, json, dateparser, timeago, requests, os
+import discord, dateparser, aiohttp
 from discord.ext import commands, tasks
 import cogs.utils.mpk as mpku
-from typing import Optional
+from typing import Optional, Union
 from datetime import datetime, timedelta
 from asyncio import sleep
 from numpy import clip
 
+def limitdatetime(dt):
+    return datetime.combine(dt.date(), datetime.min.time())
+
 class Miscellaneous(commands.Cog):
     def __init__(self, bot: commands.Bot):
+        # pylint: disable=no-member
         self.bot = bot
+        self.lastdate = limitdatetime(datetime.utcnow()) #get rid of the time
+        self.lastsromg = self.lastdate
+        self.garfloop.start()
+
+    def cog_unload(self):
+        # pylint: disable=no-member
+        self.garfloop.cancel()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # pylint: disable=no-member
+        if (self.garfloop.get_task()): self.garfloop.cancel()
+        self.garfloop.start()
 
     @commands.command(aliases = ['usrprefix', 'userprefix'])
     async def prefix(self, ctx, prefix: Optional[str] = None):
@@ -99,81 +116,136 @@ class Miscellaneous(commands.Cog):
                     return await x.edit(color=discord.Color(brighten))
         except discord.Forbidden: return
 
-    def calcstripfromdate(self, date: datetime, sromg): 
+    async def getcode(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                return resp.status
+    
+    async def calcstripfromdate(self, date: Union[datetime, int], sromg): 
+        attempts = 0
         if not sromg: 
-            while True:
-                url = f"https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/{date.year}/{date.strftime('%Y-%m-%d')}.gif"
-                if requests.head(url).status_code == 200: return (url, date)
+            while attempts <= 50:
                 url = f"http://strips.garfield.com/iimages1200/{date.year}/ga{date.strftime('%y%m%d')}.gif"
-                if requests.head(url).status_code == 200: return (url, date)
-                date -= timedelta(days=1)
-        else:
-            days = (date - datetime(2010, 1, 25)).days
-            #this is the day it started being consistent (i hope)
-            stripnum = days + 250
-            while True:
-                url = f"https://www.mezzacotta.net/garfield/comics/{stripnum}.png"
-                if requests.head(url).status_code == 200: return (url, stripnum)
-                stripnum -= 1
-
-    @commands.command(aliases=['gstrip', 'g', 'sromg'], hidden=True)
-    async def garfield(self, ctx: commands.Context, *, date: Optional[str]):
-        if not date:
-            date = datetime.utcnow() + timedelta(days=1)
-            date = datetime.combine(date.date(), datetime.min.time()) #get rid of the time
-        else:
-            msg = await ctx.send("Parsing date...")
-            date = dateparser.parse(date, settings={'STRICT_PARSING': True})
-            await msg.delete()
-            if not date: return await ctx.send("Could not parse the date given.")
-            if date > datetime.utcnow() + timedelta(days=1): return await ctx.send("Please send a date that is not in the far future (1 day max).")
-        isSROMG = ctx.message.content.startswith(f"{ctx.prefix}sromg")
-        while (date > datetime.utcnow()): date -= timedelta(days=1)
-        url, datenum = self.calcstripfromdate(date, isSROMG)
-        if not isSROMG: date = datenum
-        embed = discord.Embed(title=f"{'Garfield' if not isSROMG else 'SROMG'} Comic", colour=discord.Color(0xfe9701))
-        embed.set_footer(text=f"Strip from {timeago.format(date, datetime.utcnow())} | {date.month}/{date.day}/{date.year}")
-        embed.set_image(url=url)
-        await ctx.send(embed=embed)
-
-    @tasks.loop(minutes=3) #hacky but do not care
-    async def garfloop(self):
-        return #nothing for now
-        gids = []
-        for x in os.listdir("config"):
-            if os.path.isfile(f"config/{x}/misc.json"):
-                gids.append(x)
-        for gid in gids:
-            guild = self.bot.get_guild(int(gid))
-            if (guild == None): continue
-            #we dont need a json manager we're literally only gonna read
-            dat = json.load(open(f"config/{gid}/misc.json"))
-            try: dat['garfield']
-            except: continue
-            try: 
-                dat['garfield']['channel']
-                setting = dat['garfield']['setting']
-            except: continue
-            if not setting & 0b100:
-                try: dat['garfield']['time']
-                except: continue       
-            chn = guild.get_channel(dat['garfield']['channel'])
-            date = datetime.utcnow() + timedelta(days=1)
-            date = datetime.combine(date.date(), datetime.min.time()) #get rid of the time
-            gembed = sembed = None
-            gembed 
-            chn 
-            sembed
-            if setting & 0b001:
-                date -= timedelta(days=1)
+                if (await self.getcode(url)) == 200: return (url, date)
                 url = f"https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/{date.year}/{date.strftime('%Y-%m-%d')}.gif"
-                if requests.head(url).status_code == 200: continue
-                url = self.calcstripfromdate(date, False)
-                embed = discord.Embed(title="Garfield Comic", colour=discord.Color(0xfe9701))
-                embed.set_footer(text=f"Strip from {timeago.format(date, datetime.utcnow())} | {date.month}/{date.day}/{date.year}")
-                embed.set_image(url=url)
+                if (await self.getcode(url)) == 200: return (url, date)
+                date -= timedelta(days=1)
+                attempts += 1
+        else:
+            #this is the day it started being consistent (i hope)
+            if type(date) != int:
+                stripnum = (date - datetime(2010, 1, 25)).days + 251
+            else: stripnum = date
+            maxnum = (datetime.utcnow() - datetime(2010, 1, 25)).days + 251
+            while attempts <= 50:
+                url = f"https://www.mezzacotta.net/garfield/comics/{stripnum:04}.png"
+                if (await self.getcode(url)) == 200: return (url, limitdatetime(datetime.utcnow()) - timedelta(days=(maxnum - stripnum)))
+                stripnum -= 1
+                attempts += 1
+        return (-1, datetime.utcnow())
 
- 
+    def formatembed(self, url, s, d, day=None):
+        embed = discord.Embed(title=f"{'Daily ' if d else ''}{'SROMG' if s else 'Garfield'} Comic", colour=discord.Color(0xfe9701))
+        if s: 
+            num = url.split('/')[-1][:-4]
+            embed.set_footer(text=f"Strip #{int(num)}")
+            embed.description = f"View the details of the strip [here.](https://www.mezzacotta.net/garfield/?comic={num})"
+        else:
+            embed.set_footer(text=f"Strip from {day.month}/{day.day}/{day.year}")
+        embed.set_image(url=url)
+        return embed
+        
+    @commands.command(aliases=['gstrip', 'garf', 'sromg'])
+    async def garfield(self, ctx: commands.Context, *, date: Optional[str]):
+        """Shows a Garfield or SROMG strip. You can also subscribe to daily strips (including DMs).
+        Subscribing to a **channel** (not DMs) requires you to have **manage channel** for that channel.
+
+        `garfield/gstrip/g/sromg [date]`
+        `g/sromg sub/subscribe`"""
+        isSROMG = ctx.message.content.startswith(f"{ctx.prefix}sromg")
+        num = 0
+        if not date:
+            date = limitdatetime((datetime.utcnow() + timedelta(days=1)))
+        elif date.startswith("sub"):
+            if ctx.guild:
+                if not ctx.author.permissions_in(ctx.channel).manage_channels: raise commands.MissingPermissions(["manage_channel"])
+                mpm = mpku.getmpm("misc", ctx.guild)
+                mpk = mpm.data
+            else:
+                mpm = mpku.MPKManager('users', None)
+                mpk = mpm.data[str(ctx.author.id)]
+            try: mpk['garfield']
+            except: mpk['garfield'] = {'g': 0, 's': 0}
+            check = 's' if isSROMG else 'g'
+            if (not mpk['garfield'][check]) or (ctx.guild and mpk['garfield'][check] != ctx.channel.id):
+                mpk['garfield'][check] = ctx.channel.id if ctx.guild else 1
+                mpm.save()
+                return await ctx.send(f"{'This channel' if ctx.guild else 'You'} will now recieve {'SROMG' if isSROMG else 'Garfield'} strips daily!")
+            mpk['garfield'][check] = 0
+            mpm.save()
+            return await ctx.send(f"{'This channel' if ctx.guild else 'You'} will no longer recieve {'SROMG' if isSROMG else 'Garfield'} strips.")
+        else:
+            if isSROMG: 
+                try: num = int(date)
+                except: pass
+                else:
+                    if num <= 0:
+                        return await ctx.send("Please send a valid SROMG strip #.")
+            if not num:
+                msg = await ctx.send("Parsing date...")
+                date = dateparser.parse(date, settings={'STRICT_PARSING': True})
+                await msg.delete()
+                if not date: return await ctx.send("Could not parse the date given.")
+                if date > datetime.utcnow() + timedelta(days=1): return await ctx.send("Please send a date that is not in the far future (1 day max).")
+        if not num: 
+            while (date > (datetime.utcnow() + timedelta(days=1))): date -= timedelta(days=1)
+        else: date = num
+        url, date = await self.calcstripfromdate(date, isSROMG)
+        if (url == -1):
+            return await ctx.send("Could not find that strip.")
+        await ctx.send(embed=self.formatembed(url, isSROMG, False, date))
+
+    @tasks.loop(minutes=5, reconnect=True)
+    async def garfloop(self):
+        print("gstart")
+        gurl, gdate = await self.calcstripfromdate(datetime.utcnow() + timedelta(days=1), False)
+        surl, sdate = await self.calcstripfromdate(datetime.utcnow() + timedelta(days=1), True)
+        shown = 0b00
+        if (gurl != -1) and gdate > self.lastdate:
+            shown |= 0b01
+            self.lastdate = gdate
+        if (surl != -1) and sdate > self.lastsromg:
+            shown |= 0b10
+            self.lastsromg = sdate
+        print(shown)
+        #if not shown: return
+        #first we check guilds cause thats easy
+        for guild in self.bot.guilds:
+            guild: discord.Guild
+            try: mpk = mpku.getmpm('misc', guild).data['garfield']
+            except: continue
+            if (shown & 0b01) and mpk['g']:
+                chn = guild.get_channel(mpk['g'])
+                if chn:
+                    await chn.send(embed=self.formatembed(gurl, False, True, gdate))
+            if (shown & 0b10) and mpk['s']:
+                chn = guild.get_channel(mpk['s'])
+                if chn:
+                    await chn.send(embed=self.formatembed(surl, True, True, sdate))
+        mpkr = mpku.MPKManager("users", None).data
+        for uid in mpkr:
+            try: mpk = mpkr[uid]['garfield']
+            except: continue
+            user: discord.User = self.bot.get_user(uid)
+            if not user: user = await self.bot.fetch_user(uid)
+            if not user: continue
+            if (shown & 0b01) and mpk['g']:
+                try: await user.send(embed=self.formatembed(gurl, False, True, gdate))
+                except: continue
+            if (shown & 0b10) and mpk['s']:
+                try: await user.send(embed=self.formatembed(surl, True, True, sdate))
+                except: continue
+
 
 def setup(bot):
     bot.add_cog(Miscellaneous(bot))
