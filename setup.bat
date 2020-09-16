d10f94218cf421c547b892afe74adb135d5c6019
RMGRich <rmgrichdeveloper@gmail.com>
2020-09-16 13:26:16 -0500

    EUGH!!!

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
 py -3 -m pip install --upgrade pip
-py -3 -m pip install -U discord timeago msgpack dateparser pillow numpy aiohttp pytimeparse git+https://github.com/Rapptz/discord-ext-menus
+py -3 -m pip install -U discord.py timeago msgpack dateparser pillow numpy aiohttp pytimeparse git+https://github.com/Rapptz/discord-ext-menus
