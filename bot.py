04f1f7d9525044d0057c9683a66767bb3ebafd41
RMGRich <rmgrichdeveloper@gmail.com>
2020-09-17 18:36:30 -0500

    zzzZZ

--- bot.py
+++ bot.py
@@ -121,7 +121,7 @@ async def on_command_error(ctx: commands.Context, error):
 redirect = False
 iteration = 0
 count = 181
-hourcounter = 0
+hourcounter = 3600
 @tasks.loop(seconds=1, reconnect=True)
 async def redirloop(): #also the global loop
     ####EDIT LOOP
