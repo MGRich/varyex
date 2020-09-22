f8d183070bc0112c92dcecb65db7c8dd57404fe0
RMGRich <rmgrichdeveloper@gmail.com>
2020-09-22 18:26:28 -0500

    thats dumb

--- updater.py
+++ updater.py
@@ -7,8 +7,8 @@ try:
 except FileNotFoundError: pass
 stout = open("updateout.log", "w+")
 stdr = open("updateerr.log", "w+")
-subprocess.Popen(['git', 'checkout', 'master'], stdout=stout, stderr=stdr, creationflags=subprocess.CREATE_NO_WINDOW)
-subprocess.Popen(['git', 'pull'], stdout=stout, stderr=stdr, creationflags=subprocess.CREATE_NO_WINDOW)
+subprocess.Popen(['git', 'checkout', 'master'], stdout=stout, stderr=stdr, creationflags=134217728)
+subprocess.Popen(['git', 'pull'], stdout=stout, stderr=stdr, creationflags=134217728)
 stout.close()
 stdr.close()
 pid = subprocess.Popen([sys.executable, "bot.py"] + sys.argv[1:], creationflags=0x10).pid
