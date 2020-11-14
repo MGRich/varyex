336f205543442629d4b201fbd75419d14f5828f0
RMGRich <rmgrichdeveloper@gmail.com>
2020-11-13 23:36:58 -0600

    IM BOOORRNN

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
 py -3.8 -m pip install --upgrade pip
-py -3.8 -m pip install -U python-dotenv discord.py git+https://github.com/Rapptz/discord-ext-menus u-msgpack-python aiohttp timeago dateparser pillow numpy pytimeparse pygithub pytz
+py -3.8 -m pip install -U python-dotenv discord.py git+https://github.com/Rapptz/discord-ext-menus u-msgpack-python aiohttp timeago dateparser pillow numpy pytimeparse pygithub pytz humanize
